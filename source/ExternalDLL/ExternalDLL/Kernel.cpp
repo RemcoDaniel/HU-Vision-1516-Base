f31397be8ee25adf1079a8cbfd7f4084d3ccdf0f
RemcoDaniel <remco.nijkamp@student.hu.nl>
2016-05-20 16:11:09 +0200

    added fill...Kernel functions to Kernel class

--- source/ExternalDLL/ExternalDLL/Kernel.cpp
+++ source/ExternalDLL/ExternalDLL/Kernel.cpp
@@ -1,49 +1,55 @@
 #include "Kernel.h"
 
-Kernel::Kernel(int width, int height, int mode) {
+Kernel::Kernel(int width, int height, int mode, int strength) {		// strength kan van -10 tot 10 lopen
 	// eerst checken of beiden wel oneven (anders geen middelste pixel :( )
 	if (width % 2 && height % 2) {	// misschien sneller als eerst size berekenen en dan checken?
 		fillKernel(width, height, mode);
 	}
 }
 
-void Kernel::fillKernel(int witdth, int height, int mode) {
+void Kernel::fillKernel(int witdth, int height, int mode, int strength) {
 	switch (mode) {
-	case 1:		fillNormalKernel(width, height);	break;		// Normal
-	case 2:		fillLaplacianKernel(width, height);	break;		// Laplacian
-	case 3:		fillPrewittKernel(width, height);	break;		// Prewitt
-	case 4:		fillSobelKernel(width, height);		break;		// Sobel
-	case 5:		fillGaussianKernel(width, height);	break;		// Gaussian
-	default:	std::cout << "value of x unknown";	break;
+		case 1:		fillNormalKernel(width, height, strength);		break;		// Normal
+		case 2:		fillLaplacianKernel(width, height, strength);	break;		// Laplacian
+		case 3:		fillPrewittKernel(width, height, strength);		break;		// Prewitt
+		case 4:		fillSobelKernel(width, height, strength);		break;		// Sobel
+		case 5:		fillGaussianKernel(width, height, strength);	break;		// Gaussian
+		default:	std::cout << "value of x unknown";				break;
 	}
 }
 
-void Kernel::fillNormalKernel(int width, int height) {	// deze vult de kernel met waarden
+void Kernel::fillNormalKernel(int width, int height, int strength) {	// deze vult de kernel met waarden
 	int size = width * height;
 	data = new int[size];
 
-	double coeff = 0.04;
 	for (int i = 0; i < size; ++i){
-		data[i] = coeff;
+		data[i] = strength;
 	}
 }
 
-void fillLaplacianKernel(int width, int height){
+void fillLaplacianKernel(int width, int height, int strength){
+	int size = width * height;
+	data = new int[size];
+
+
+}
+
+void fillPrewittKernel(int width, int height, int strength){
+	int size = width * height;
+	data = new int[size];
+
+}
+
+void fillSobelKernel(int width, int height, int strength){
 	int size = width * height;
 	data = new int[size];
 }
 
-void fillPrewittKernel(int width, int height){
+/*
+void fillGaussianKernel(int width, int height, int strength){
 	int size = width * height;
 	data = new int[size];
-}
-
-void fillSobelKernel(int width, int height){
-	int size = width * height;
-	data = new int[size];
-}
-
-void fillGaussianKernel(int width, int height){
-	int size = width * height;
-	data = new int[size];
-}
+	for (int i = 0; i < size; ++i){
+		data[i] = <int>((1 / (2 * PI * strength ^ 2)) * E ^ ((width ^ 2 + height ^ 2) / (2 * strength ^ 2)); // strength is hier dus de deviation!! let op!: ff kijken of strength dan wel de juiste waarde heeft!  strength wil je hier eigenlijk tussen de 0.0 en de 1.0
+	}
+} */
--- source/ExternalDLL/ExternalDLL/Kernel.h
+++ source/ExternalDLL/ExternalDLL/Kernel.h
@@ -1,16 +1,19 @@
 #include <iostream>
 
+#define PI 3.14159265359
+#define E  2.718281828459
+
 class Kernel {
 public:
 	// mode kan zijn: Laplacian, Prewitt, Sobel, Gaussian
-	Kernel(int width, int height, int mode);	// mode moet geen int zijn, maar enum
+	Kernel(int width, int height, int mode, int strength);	// mode moet geen int zijn, maar enum
 	~Kernel();
-	void fillKernel(int width, int height, int mode);
-	void fillNormalKernel(int width, int height);
-	void fillLaplacianKernel(int width, int height);
-	void fillPrewittKernel(int width, int height);
-	void fillSobelKernel(int width, int height);
-	void fillGaussianKernel(int width, int height);
+	void fillKernel(int width, int height, int mode, int strength);
+	void fillNormalKernel(int width, int height, int strength);
+	void fillLaplacianKernel(int width, int height, int strength);
+	void fillPrewittKernel(int width, int height, int strength);
+	void fillSobelKernel(int width, int height, int strength);
+	void fillGaussianKernel(int width, int height, int strength);
 
 	const int getWidth() const;
 	const int getHeight() const;
